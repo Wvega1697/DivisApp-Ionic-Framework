cb402278efd5244e652f29f2319fbe5189857394
WVEGA1697 <williamsv1697@gmail.com>
2024-11-17 12:40:55 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'divisapp-22011423',
+  webDir: 'www'
+};
+
+export default config;
