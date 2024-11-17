f546b72a2daa4b0ea1777984f55721de594d6ac8
WVEGA1697 <williamsv1697@gmail.com>
2024-11-17 12:55:23 -0600

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  cantidadQuetzales: number | null = null;
+  resultado: number | null = null;
+  tasaCambio: number = 8; // 8 quetzales por dólar
 
   constructor() {}
 
+  convertir() {
+    if (this.cantidadQuetzales !== null && this.cantidadQuetzales > 0) {
+      this.resultado = this.cantidadQuetzales * this.tasaCambio;
+    } else {
+      this.resultado = null;
+    }
+  }
 }
